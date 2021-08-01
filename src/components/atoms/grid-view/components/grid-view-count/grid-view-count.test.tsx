b1a32dbbcf97effbb5092df197e3c829d40735fb
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2021-08-01 10:59:49 +0200

    :sparkles: Add GridView component

---
+++ src/components/atoms/grid-view/components/grid-view-count/grid-view-count.test.tsx
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+import Colors from '../../../colors';
+import GridViewCount from './grid-view-count';
+
+const items = Array.from({ length: 32 }, (_, index) => {
+  const color = Colors.primaries[Math.floor(Math.random() * Colors.primaries.length)];
+
+  return (
+    <div key={index} style={{ backgroundColor: color['400'].toRGBA() }}>
+      Item {index}
+    </div>
+  );
+});
+
+it('should render successfully', () => {
+  render(<GridViewCount crossAxisCount={4}>{items}</GridViewCount>);
+});
---
+++ src/components/atoms/grid-view/grid-view.test.tsx
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+
+import GridView from './grid-view';
+
+it('should render successfully', () => {
+  render(<GridView />);
+});
