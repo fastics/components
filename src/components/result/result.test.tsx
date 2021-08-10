4584e33ff1397af03e0cd6b073f11f9e4f2ac1f4
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2021-08-10 18:31:34 +0200

    fix: fixed TypeScript and test

--- src/components/result/result.test.tsx
+++ src/components/result/result.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 
-import Result from './result';
+import Result, { ResultStatus } from './result';
 
 it('should render successfully', () => {
-  render(<Result />);
+  render(<Result title="Hello Testing World !" status={ResultStatus.SUCCESS} />);
 });
