f5b81d03d1e1838fdf3663ceb5a7950a0522e15e
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2021-08-18 09:47:45 +0200

    test(coverage): improve GridView.Count coverage

--- src/components/grid-view/components/grid-view-count/grid-view-count.test.tsx
+++ src/components/grid-view/components/grid-view-count/grid-view-count.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 
 import Colors from '../../../colors';
-import GridViewCount from './grid-view-count';
+import GridViewCount, { Axis } from './grid-view-count';
 
 const items = Array.from({ length: 32 }, (_, index) => {
   const color = Colors.primaries[Math.floor(Math.random() * Colors.primaries.length)];
@@ -13,6 +13,16 @@ const items = Array.from({ length: 32 }, (_, index) => {
   );
 });
 
-it('should render successfully', () => {
-  render(<GridViewCount crossAxisCount={4}>{items}</GridViewCount>);
+describe('GridView.Count', () => {
+  it('should render successfully', () => {
+    render(<GridViewCount crossAxisCount={4}>{items}</GridViewCount>);
+  });
+
+  it('should render successfully with horizontal scroll', () => {
+    render(
+      <GridViewCount scrollDirection={Axis.horizontal} crossAxisCount={4}>
+        {items}
+      </GridViewCount>,
+    );
+  });
 });
