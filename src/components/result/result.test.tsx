403cf8a560f953da9b1c545b71456b64c688d8a2
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2021-08-18 09:53:30 +0200

    test(coverage): improve Result coverage

--- src/components/result/result.test.tsx
+++ src/components/result/result.test.tsx
@@ -2,6 +2,39 @@ import { render } from '@testing-library/react';
 
 import Result, { ResultStatus } from './result';
 
-it('should render successfully', () => {
-  render(<Result title="Hello Testing World !" status={ResultStatus.SUCCESS} />);
+describe('Result', () => {
+  it('should render successfully with success', () => {
+    render(<Result title="Hello Testing World!" status={ResultStatus.SUCCESS} />);
+  });
+
+  it('should render successfully with info', () => {
+    render(<Result title="Your operation has been executed." status={ResultStatus.INFO} />);
+  });
+
+  it('should render successfully with warning', () => {
+    render(
+      <Result title="There are some problems with your operation." status={ResultStatus.WARNING} />,
+    );
+  });
+
+  it('should render successfully with error', () => {
+    render(
+      <Result
+        title="Submission Failed"
+        subtitle="Please check and modify the following information before resubmitting."
+        status={ResultStatus.ERROR}
+      />,
+    );
+  });
+
+  it('should render successfully with unknown status', () => {
+    render(
+      <Result
+        title="Submission Failed"
+        subtitle="Please check and modify the following information before resubmitting."
+        // @ts-expect-error
+        status="unknown"
+      />,
+    );
+  });
 });
