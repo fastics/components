d063c35eab6b62dbd35efde0c727193d9e44cb07
Donovan BENFOUZARI <d.benfouzari@gmail.com>
2021-08-18 09:49:10 +0200

    test(coverage): improve LinearProgressIndicator coverage

--- src/components/linear-progress-indicator/linear-progress-indicator.test.tsx
+++ src/components/linear-progress-indicator/linear-progress-indicator.test.tsx
@@ -2,6 +2,14 @@ import { render } from '@testing-library/react';
 
 import LinearProgressIndicator from './linear-progress-indicator';
 
-it('should render successfully', () => {
-  render(<LinearProgressIndicator value={40} />);
+describe('LinearProgressIndicator', () => {
+  it('should render successfully', () => {
+    render(<LinearProgressIndicator value={40} />);
+  });
+
+  it('should throw when value > 100', () => {
+    expect(() => {
+      render(<LinearProgressIndicator value={140} />);
+    }).toThrow('[LinearProgressIndicator] - The value should not exceed 100');
+  });
 });
